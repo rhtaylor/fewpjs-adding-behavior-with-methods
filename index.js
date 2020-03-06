08537efbaf8ce2870c5a207807e25185b48dc90e
Randy <randyisbusy@gmail.com>
2020-03-06 13:10:57 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Your code here
+class Cat {
+    constructor(name,sex){
+        this.name = name;
+        this.sex = sex;
+    }
+    speak(){
+        return ``
+    }
+} 
+class Dog {
+    constructor(name, sex) {
+        this.name = name;
+        this.sex = sex;
+    }
+    speak() {
+        return ``
+    }
+} 
+class Bird {
+    constructor(name, sex) {
+        this.name = name;
+        this.sex = sex;
+    }
+    speak() {
+        return ``
+    }
+}
